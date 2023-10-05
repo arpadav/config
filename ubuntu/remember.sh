a4e8c51b9d7aa219cef00b3a28c42ca9a5165960
Arpad Voros <arpadav@gmail.com>
2023-10-05 12:54:53 -0400

    OS specific, easier to move/copy

---
+++ ubuntu/remember.sh
@@ -0,0 +1,18 @@
+# ssh conifg with github
+chmod 600 ~/.ssh/config
+: '
+```~/.ssh/config
+Host github.com
+    Hostname ssh.github.com
+    Port 443
+    User git
+```
+'
+
+# Nvim installation
+cd ~/installations
+wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+tar xzvf nvim-linux64.tar.gz
+cd nvim-linux64
+sudo cp -r * /usr/
+cd ~
