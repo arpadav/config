105694be64ae5f380cbc4f421e191fd98fb9efcb
Arpad Voros <arpad.voros@jhuapl.edu>
2023-10-05 13:46:12 -0400

    mistake with init sequence, some changes

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
-find . -type f -name ".init~" -exec rm -f {} \;
-shopt -s extglob
+
+# constants
+MAX_DEPTH=2
+INIT_NAME=".init~"
+
+# remove all directory init files
+find . -maxdepth $MAX_DEPTH -type f -name "$INIT_NAME" -exec rm -f {} \;
+
+# copy all hidden dirs 
+cd copy
+cp -r .[^.]* ~/
+cd - > /dev/null 2>&1
+
+# copy all remaining contents
+# shopt -s extglob
 cp -r copy/* ~/
+
+# echo off, re-init directory init files
+find . -maxdepth $MAX_DEPTH -type d | while read -r dir; do
+    if [[ ! "$(ls -A "$dir")" ]]; then
+        touch "$dir/$INIT_NAME"
+    fi
+done
+
+# unset all
+unset MAX_DEPTH
+unset INIT_NAME
