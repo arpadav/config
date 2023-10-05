51b56bbf8b26f11f3dbbc077825e4f89cab7e07f
Arpad Voros <arpadav@gmail.com>
2023-10-05 12:29:00 -0400

    remove readme before move on init

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
 SCRIPT_NAME="$(basename "$SCRIPT_PATH")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 shopt -s extglob
+rm "$SCRIPT_DIR/README.md"
 mv "$SCRIPT_DIR"/!($SCRIPT_NAME) ~/
 rm -- "$SCRIPT_PATH"
 unset SCRIPT_PATH
