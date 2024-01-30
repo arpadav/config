3f9642f30922af51d36239dd997a28be43c431df
Arpad Voros <arpad.voros@jhuapl.edu>
2024-01-30 11:37:28 -0500

    added git contrib script

--- ubuntu/add_installation_to_path.sh
+++ ubuntu/add_installation_to_path.sh
@@ -5,8 +5,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # loop through sub directories
 for d in $DIR/*/ ; do
-    # add to path
-    export PATH=$PATH:$d
+    # if there are any binary files in this folder, add to path
+    if [ -f "$d"*.bin ]; then
+        export PATH=$PATH:$d
+    fi
     # if there is a folder called 'bin', add it to path
     if [ -d "$d/bin" ]; then
         export PATH=$PATH:$d/bin
